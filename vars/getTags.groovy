bf8bd99c81784ce9fdf811e18c67b334e39299ac
dstieglitz <dstieglitz@stainlesscode.com>
2020-04-22 08:20:47 -0700

    added a getTags utility to ensure up-to-date tags (fix)

--- vars/getTags.groovy
+++ vars/getTags.groovy
@@ -1,7 +1,8 @@
 /**
  * Force a fetch of tags.
  * This must be called within a valid withCredentials {} block to work.
+ * Provided to workaround Jenkins builds that can't fetch tags for some reason.
  */
-def call(Map config) {
-    sh 'git fetch --all --tags -p -P'
+def call(String gitRepo, String username, String password) {
+    sh "git fetch https://${username}:${password}@${gitRepo} --tags -p -P"
 }
